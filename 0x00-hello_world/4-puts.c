279ccc70585ab454836112415422ca061acf8f20
abel-udoh <abeludoh8@gmail.com>
2022-12-10 23:09:29 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 /**
- * Main - Entry
- *
- * Description: prints "Programming is like building a multilingual puzzle"
+ * Main - Entry point 
  *
  * Return: Always 0 (Success)
+ *
  */
-int main (void)
+int main(void)
 {
-
 	puts("\"Programming is like building a multilingual puzzle);
 	return (0);
-
 }
