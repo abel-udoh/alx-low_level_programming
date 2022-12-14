ac02fd5a15d95602c32e806911e94c12571fe2f7
abel-udoh <abeludoh8@gmail.com>
2022-12-14 05:44:01 -0800

    4 alphabets

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 
 /**
- * main - Entry point
+ * main - print the letters of the alphabet
+ *
+ * Description: print the letters of the alphabet except e, q
  *
  * Return: Always 0 (Success)
 */
@@ -12,13 +14,15 @@ int main(void)
 	int i = 97;
 
 	while (i > 123)
+
 	{
 		if (i != 101 && i != 113)
 		{
 			putchar(i);
-			i++;
-
 		}
+
+		i++;
+
 	}
 
 	putchar(10);
