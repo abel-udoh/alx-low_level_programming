f06edd54b0c6742edf047e42f173fd3de3593cb5
abel-udoh <abeludoh8@gmail.com>
2022-12-14 04:49:46 -0800

    4 print

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i = 97;
+
+	while (i > 123)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+			i++;
+
+		}
+	}
+
+	putchar(10);
+
+	return (0);
+}
