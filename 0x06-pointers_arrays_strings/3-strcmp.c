95a14f4eb5f810f1427c95dd7f7ef202e2ccaca5
abel-udoh <abeludoh8@gmail.com>
2022-12-21 19:30:12 -0800

    task 4

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare two string
+ * @s1:string1
+ * @s2:string2
+ * return: compare _strcmp
+ */
+
+int _strcmp(char *s1, char *s2);
+{
+
+	int s1, s2;
+
+		s1 = "hello";
+		s2 = "world";
+
+		if s1 == s2 :
+			return True
+		else
+			return False
+
+
+				are_equal = compare_strings(s1, s2)
+
+				if are_equal :
+					_putchar("The strings are equal.");
+				else
+					_putchar("The strings are not equal.");
+
+		return (compare);
+}
