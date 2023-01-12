9508880e7d524a6f701cb53e58cac8a871915d35
abel-udoh <abeludoh8@gmail.com>
2023-01-11 20:18:40 -0800

    static library

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenate two strings
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+
+{
+
+}
---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strchr - Entry point
+ * @s: input value
+ * @c: input value
+ * Return: Always 0 (Success)
+ */
+
+char *_strchr(char *s, char c)
+
+{
+
+}
