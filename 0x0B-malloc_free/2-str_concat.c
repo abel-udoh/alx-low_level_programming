c080643ea20edafbf24bbf9d14d1bacf5d60259a
abel-udoh <abeludoh8@gmail.com>
2023-01-16 19:54:16 -0800

    task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * str_concat - function that concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: point to a newly allocated space in memory, NULL if failed
+ */
+
+char *str_concat(char *s1, char *s2)
+
+{
+	int i = 0, j = 0, l = 0, k = 0;
+
+	char *s;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[i])
+	i++;
+
+	while (s2[j])
+	j++;
+
+	l = i + j;
+	s = (char *)malloc(l * sizeof(char) + 1);
+	if (s == NULL)
+		return (NULL);
+
+	while (k < l)
+
+	{
+		if (k < i)
+			s[k] = s1[k];
+		if (k >= i)
+		{
+			s[k] = s2[j];
+			j++;
+		}
+		k++;
+	}
+		s[k] = '\0';
+		return (s);
+}
