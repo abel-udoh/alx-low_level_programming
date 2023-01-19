37bcbeeee85b022da0dc69708f0332cd8213b2ca
abel-udoh <abeludoh8@gmail.com>
2023-01-18 18:25:10 -0800

    task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,13 +17,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (array == NULL || cmp == NULL)
 		return (-1);
+
 	if (size <= 0)
 		return (-1);
+
 	for (i = 0; i < size; i++)
 	{
 		result = cmp(array[i]);
+
 		if (result == 1)
-			return (1);
+			return (i);
 	}
 	return (-1);
 }
