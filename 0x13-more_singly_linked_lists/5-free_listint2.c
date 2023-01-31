b3d9f7009a642a83d6bb7dce131e2a8fe1704fb6
abel-udoh <abeludoh8@gmail.com>
2023-01-30 22:28:59 -0800

    task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,21 +2,24 @@
 #include <stdio.h>
 
 /**
- * free_listint - free a listint list
+ * free_listint2 - free a listint list
  * @head: head of a linked list
  * Return: void
  */
-void free_listint(listint_t *head)
+
+void free_listint2(listint_t **head)
 
 {
 	listint_t *temp;
 
-	temp = head;
+	if (head == NULL)
+		return;
+	temp = *head;
 	while (temp != NULL)
 	{
 		temp = temp->next;
 		free(head);
-		head = temp;
+		*head = temp;
 	}
+	*head = NULL;
 }
-
