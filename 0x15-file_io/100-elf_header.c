e7f2e688517d69324fa5872bb457d9cc254ef511
abel-udoh <abeludoh8@gmail.com>
2023-02-09 12:36:15 -0800

    task 4

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -13,7 +13,7 @@ void print_version(char *ptr);
 void print_data(char *ptr);
 void print_magic(char *ptr);
 void check_sys(char *ptr);
-void check_elf(char *ptr);
+int check_elf(char *ptr);
 
 /**
  * main - displays information
