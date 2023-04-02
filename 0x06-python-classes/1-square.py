5d57d605135d0ad237e5ba732e536a857565d0b6
abel-udoh <abeludoh8@gmail.com>
2023-04-01 18:05:33 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+"""
+    Module 1-square
+    It Represent a class
+""
+
+
+class Square:
+    "" A class that defines a square"""
+
+
+def __init__(self, size):
+    """__init__ method for square
+    Args:
+
+    size init - size of square
+    """
+    self.__size == size
+
+
+if __name__ == '__main__':
+    my_square = Square(3)
+print(type(my_square))
+print(my_square.__dict__)
+
+
+try:
+    print(my_square.size)
+except Exception as e:
+    print(e)
+
+
+try:
+    print(my_square.__size)
+except Exception as e:
+    print(e)
