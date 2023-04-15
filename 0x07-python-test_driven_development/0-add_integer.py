07a355a695b2cac45f61fb3deecdbcdf153bc5a0
abel-udoh <abeludoh8@gmail.com>
2023-04-15 12:54:07 -0700

    tdd task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,14 +10,8 @@ def add_integer(a, b=98):
     """ add_integer - adds two integers (a, b)
     Returns: integer sum
     """
-    if not isinstance(a, int) and not isinstance(a, float):
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int) and not isinstance(b, float):
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
-
-    if isinstance(a, float):
-        a = int(a)
-    if isinstance(b, float):
-        b = int(b)
-
-    return a + b
+    return (int(a) + int(b))
