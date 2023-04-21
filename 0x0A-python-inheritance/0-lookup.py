02ef451fc4749c310c047df6496ffe47bf716d2f
abel-udoh <abeludoh8@gmail.com>
2023-04-21 08:07:07 -0700

    task 1

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,6 +3,7 @@
 A module that contains function that returns the list of available attributes
 """
 
+
 def lookup(obj):
     """function that returns the list of available attributes"""
     return dir(obj)
