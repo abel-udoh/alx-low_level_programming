da0c86a81cee3412947afa411b24f913bf96bcc6
abel-udoh <abeludoh8@gmail.com>
2023-08-13 14:12:25 -0700

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -7,7 +7,7 @@
  * @s2: second string to copy
  * @n: number of bytes of s2 to copy
  *
- * pointer to a newlly allocated memory with s1 & s2, NULL if it fail
+ * Return: pointer to a newlly allocated memory with s1 & s2, NULL if it fail
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
